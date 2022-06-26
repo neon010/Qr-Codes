a4c75b3aced50e1a71032ebc36a521dad448fac7
neon010 <mks10685@gmail.com>
2022-06-26 17:32:06 +0530

    revert back

--- android/app/src/main/java/com/mksmohan/easyqrcodescanner/MainApplication.java
+++ android/app/src/main/java/com/mksmohan/easyqrcodescanner/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+// import io.invertase.googlemobileads.ReactNativeGoogleMobileAdsPackage;
 import com.rnfs.RNFSPackage;
 import com.gevorg.reactlibrary.RNQrGeneratorPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
